ed2df8303f3fa60c4893d48751e9f2d5cd70fb9c
Raj Koirala <rajkoirala@Rajs-MacBook-Air.local>
2025-08-23 03:49:12 -0700

    change in config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://cdn.chatspark.rajkoirala.com.np/"
-      : "", // no prefix in dev
-  images: {
-    domains: ["cdn.chatspark.rajkoirala.com.np"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
